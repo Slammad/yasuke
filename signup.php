8c18f6b58a7c14f871c9d07c037f285507fa9fd4
Slammad <saidubello212@gmail.com>
2018-10-15 18:57:07 +0100

    asd

---
+++ signup.php
@@ -0,0 +1,66 @@
+<?php
+    include 'admin/sign.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="assets/img/basic/favicon.ico" type="image/x-icon">
+    <title>mini project 1</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/app.css">
+ 
+</head>
+<body class="light">
+<!-- Pre loader -->
+
+<div id="app">
+<main>
+    <div id="primary" class="p-t-b-100 height-full">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 mx-md-auto paper-card">
+                    <div class="text-center">
+                        <img src="assets/images/ping.png" style="height: 100px;" alt="">
+                        <h3 class="mt-2">YASUKE ON CHARTS</h3>
+                        <p class="p-t-b-20">Welcome to YasukeOnCharts Sign up to get more contents</p>
+                    </div>
+                    <form action="index.php" method="POST">
+                        <div class="form-group has-icon"><i class="icon-bank"></i>
+                            <input type="text" name='ver' class="form-control form-control-lg"
+                                   placeholder="Serial Number">
+                        </div>
+                       
+                        <input type="submit"  name="submit" class="btn btn-primary btn-lg btn-block" value="Verify Drug">
+                       
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- #primary -->
+</main>
+<div class="control-sidebar-bg shadow white fixed"></div>
+</div>
+<!--/#app -->
+
+</body>
+</html>
+
+<?php
+   
+        if(isset($_POST['submit'])) {
+       
+        $sn=$_POST['ver'];
+        
+        echo $sn;
+        }
+
+  
+
+?>
