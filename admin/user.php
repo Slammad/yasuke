0f17a7defaacdc502996d869f2cb70bdd782d52d
Slammad <saidubello212@gmail.com>
2018-10-15 22:50:49 +0100

    fasd

--- admin/user.php
+++ admin/user.php
@@ -11,8 +11,8 @@
 
 <section class="content">
 
-  <div class="row hidden">
-    <div class="col-md-12">
+  <div class="row">
+    <div class="col-md-12 hidden">
 
       <div class="box box-info">
         
